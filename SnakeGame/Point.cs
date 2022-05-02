7edc149c227e15c04dc562f9213aa2007b28290c
Ravil <34988095+RavilBoyukzade@users.noreply.github.com>
2022-05-02 16:03:14 +0400

    added  construction method

--- SnakeGame/Point.cs
+++ SnakeGame/Point.cs
@@ -12,7 +12,12 @@ namespace SnakeGame
         public int y;
         public char sym;
 
-
+        public Point(int _x, int _y, char _sym)
+        {
+            x=_x;
+            y=_y;
+            sym = _sym;
+        }
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
